edba9f26510f3883dca00d67b6307540b502f63c
Bardia Moshiri <fakeshell@bardia.tech>
2022-09-26 12:16:20 -0400

    add adaptation script

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/sbin/sh
+mknod /dev/loop1 b 7 8
+losetup /dev/loop1 /data/rootfs.img
+mkdir /rootfs && mount /dev/loop1 /rootfs
+cp ./etc/systemd/system/apt-fix.service /rootfs/etc/systemd/system/
+cp ./etc/systemd/system/samsung-hwc.service /rootfs/etc/systemd/system/
+cp ./etc/systemd/system/systemd-resolved.service /rootfs/etc/systemd/system/
+cp ./etc/systemd/system/systemd-timesyncd.service /rootfs/etc/systemd/system/
+cp ./etc/apt/sources.list.d/* /rootfs/etc/apt/sources.list.d/
+cp ./etc/udev/rules.d/70-exynos9810.rules /rootfs/etc/udev/rules.d/
+cp ./usr/share/keyrings/exynos9810.gpg /rootfs/usr/share/keyrings/exynos9810.gpg
+cp -r ./usr/lib/droid-vendor-overlay/ /rootfs/usr/lib/
+cp -r ./usr/bin/* /rootfs/usr/
+rm -f /rootfs/etc/resolv.conf
+cp ./etc/resolv.conf /rootfs/etc/
+cp -r ./deb/ /rootfs/
+chroot /rootfs/ /bin/bash -c 'export PATH="$PATH:/usr/bin:/usr/sbin:/bin:/sbin" && systemctl enable apt-fix samsung-hwc && systemctl mask systemd-resolved systemd-timesyncd'
+chroot /rootfs/ /bin/bash -c 'export PATH="$PATH:/usr/bin:/usr/sbin:/bin:/sbin" && dpkg -i /deb/*.deb'
+echo "Now just reboot"
