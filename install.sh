a91bef3d0da6d75c198219c4d8d1f44d7161d9be
Bardia Moshiri <fakeshell@bardia.tech>
2022-10-01 17:04:57 -0400

    fix bluetooth upower and typos

--- install.sh
+++ install.sh
@@ -6,16 +6,16 @@ cp ./etc/systemd/system/apt-fix.service /rootfs/etc/systemd/system/
 cp ./etc/systemd/system/samsung-hwc.service /rootfs/etc/systemd/system/
 cp ./etc/systemd/system/epoch.service /rootfs/etc/systemd/system/
 cp ./etc/systemd/system/altresolv.service /rootfs/etc/systemd/system/
-cp ./etc/systemd/system/systemd-resolved.service /rootfs/etc/systemd/system/
-cp ./etc/systemd/system/systemd-timesyncd.service /rootfs/etc/systemd/system/
+cp ./etc/systemd/system/upoweralt.service /rootfs/etc/systemd/system/
+cp ./etc/systemd/system/bluetoothalt.service /rootfs/etc/systemd/system/
 cp ./etc/apt/sources.list.d/* /rootfs/etc/apt/sources.list.d/
 cp ./etc/udev/rules.d/70-exynos9810.rules /rootfs/etc/udev/rules.d/
 cp ./usr/share/keyrings/exynos9810.gpg /rootfs/usr/share/keyrings/exynos9810.gpg
 cp -r ./usr/lib/droid-vendor-overlay/ /rootfs/usr/lib/
-cp -r ./usr/bin/* /rootfs/usr/
+cp -r ./usr/bin/* /rootfs/usr/bin/
 rm -f /rootfs/etc/resolv.conf
 cp ./etc/resolv.conf /rootfs/etc/
 cp -r ./deb/ /rootfs/
-chroot /rootfs/ /bin/bash -c 'export PATH="$PATH:/usr/bin:/usr/sbin:/bin:/sbin" && systemctl enable apt-fix samsung-hwc epoch altresolv && systemctl mask systemd-resolved systemd-timesyncd'
+chroot /rootfs/ /bin/bash -c 'export PATH="$PATH:/usr/bin:/usr/sbin:/bin:/sbin" && systemctl enable apt-fix samsung-hwc epoch altresolv upoweralt bluetoothalt && systemctl mask systemd-resolved systemd-timesyncd upower bluetooth'
 chroot /rootfs/ /bin/bash -c 'export PATH="$PATH:/usr/bin:/usr/sbin:/bin:/sbin" && dpkg -i /deb/*.deb'
 echo "Now just reboot"
