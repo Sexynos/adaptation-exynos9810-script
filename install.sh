9ea98e664c97525f809060a9102cd7f2e2f1fc61
Bardia Moshiri <fakeshell@bardia.tech>
2022-10-27 18:02:45 -0400

    add batman

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ cp ./etc/systemd/system/epoch.service /rootfs/etc/systemd/system/
 cp ./etc/systemd/system/altresolv.service /rootfs/etc/systemd/system/
 cp ./etc/systemd/system/upoweralt.service /rootfs/etc/systemd/system/
 cp ./etc/systemd/system/bluetoothalt.service /rootfs/etc/systemd/system/
+cp ./etc/systemd/system/batman.service /rootfs/etc/systemd/system/
 cp ./etc/apt/sources.list.d/* /rootfs/etc/apt/sources.list.d/
 cp ./etc/udev/rules.d/70-exynos9810.rules /rootfs/etc/udev/rules.d/
 cp ./usr/share/keyrings/exynos9810.gpg /rootfs/usr/share/keyrings/exynos9810.gpg
@@ -16,9 +17,10 @@ cp -r ./usr/bin/* /rootfs/usr/bin/
 rm -f /rootfs/etc/resolv.conf
 cp ./etc/resolv.conf /rootfs/etc/
 cp -r ./var/lib/bluetooth/* /rootfs/var/lib/bluetooth/
+cp -r ./var/lib/batman/ /rootfs/var/lib/batman
 cp -r ./lib/systemd/system/bluebinder.service.d/ /rootfs/lib/systemd/system/
 cp -r ./deb/ /rootfs/
 chroot /rootfs/ /bin/bash -c 'export PATH="$PATH:/usr/bin:/usr/sbin:/bin:/sbin" && rm -f /etc/systemd/system/dbus-org.bluez.service'
-chroot /rootfs/ /bin/bash -c 'export PATH="$PATH:/usr/bin:/usr/sbin:/bin:/sbin" && systemctl mask systemd-resolved systemd-timesyncd upower bluetooth && systemctl enable apt-fix samsung-hwc epoch altresolv upoweralt bluetoothalt'
+chroot /rootfs/ /bin/bash -c 'export PATH="$PATH:/usr/bin:/usr/sbin:/bin:/sbin" && systemctl mask systemd-resolved systemd-timesyncd upower bluetooth && systemctl enable apt-fix samsung-hwc epoch altresolv upoweralt bluetoothalt batman'
 chroot /rootfs/ /bin/bash -c 'export PATH="$PATH:/usr/bin:/usr/sbin:/bin:/sbin" && dpkg -i /deb/*.deb'
 echo "Now just reboot"
